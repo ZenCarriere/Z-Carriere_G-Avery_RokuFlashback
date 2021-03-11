e09b8cca4f84b1b31e6d9a0beb585d878ebdf9bc
ZenCarriere <59712949+ZenCarriere@users.noreply.github.com>
2021-03-10 19:16:06 -0500

    backend setup

---
+++ routes/api.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { createPool } = require('mysql');
+
+const router = express.Router();
+
+const connect = require("../config/sqlConfig");
+
+router.get("/", (req, res) => {
+    res.json({message: "you reached the api route"});
+})
+
+// router.get("/users", (req, res) => {
+//     res.json({message: "all users route"});
+// })
+
+// router.get("/movies", (req, res) => {
+
+//     connect.getConnection(function(err, connection) {
+//         if (err) throw err; 
+       
+//         connection.query('SELECT * FROM tbl_movies', function (error, results) {
+//           connection.release();
+       
+//           if (error) throw error;
+//           res.json(results);
+       
+
+//         });
+//       });
+// })
+
+
+// router.get("/movies/:id", (req, res) => {
+//     connect.query(`SELECT * FROM tbl_movies WHERE movies_id=${req.params.id}`, function (error, results) {
+//         if (error) throw error;
+//         res.json(results);
+//       });
+// })
+
+
+module.exports = router;
